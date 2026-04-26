5d9d67407c5af43fce81a38d98e9d6f08d8dd920
teracot637-sketch <teracot637@gmail.com>
2026-04-26 22:10:00 +0000

    слушаем сообщения через ws

--- bot.py
+++ bot.py
@@ -1,20 +1,46 @@
 import asyncio
+import logging
 import os
 
 from dotenv import load_dotenv
-from karbo import KarboBot
+from karbo import KarboBot, KarboBotWS, Message
 
 
 load_dotenv()
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+)
+log = logging.getLogger("moderator")
 
 TOKEN = os.environ["KARBO_BOT_TOKEN"]
 
 
 async def main():
     async with KarboBot(TOKEN) as bot:
+        ws = KarboBotWS(TOKEN)
         me = await bot.get_me()
-        print("я", me.name, me.bot_id)
+        bot_id = me.bot_id
+        log.info("Бот онлайн: %s id=%s", me.name, bot_id)
+
+        @ws.on_message
+        async def on_message(msg: Message):
+            if msg.user_id == bot_id:
+                return
+            nick = msg.author.nickname if msg.author else "?"
+            log.info(
+                "MSG chat=%s user=%s nick=%r: %r",
+                msg.chat_id, msg.user_id, nick,
+                (msg.content or "")[:80],
+            )
+
+        log.info("Подключаюсь к WebSocket...")
+        await ws.run_forever()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
